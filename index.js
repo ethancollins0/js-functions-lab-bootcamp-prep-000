a0d45bc6b9f8f3ebf6a122dcac2bde68270cc1d4
Ethan Collins <ethantcollins98@gmail.com>
2019-04-15 00:19:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidayTo(holiday, name)
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
 
 function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}!`
